7c6675d62bc2589ddd0251b42da39e5e5e198eb7
unknown <bhumesh.ranjaneb598@gmail.com>
2022-05-17 19:26:37 +0530

    Added: UC5_WageForMonth

--- EmpWages.php
+++ EmpWages.php
@@ -1,46 +1,56 @@
 <?php
 
-class EmpWages{
-   function welcomeMsg(){
-       echo "Welcome to employee wage problem"."\n";
-   }
-   function empCheck(){
-       $empAbsent = 0;
-       $empPresent = 1;
-       $empWagePerHour = 20;
-       $fullDayHour = 8;
-       $number =rand(0,1);
-       if($number==1){
-           echo "Employee is present"."\n";
-       }
-       else{
-           echo "Employee is Absent"."\n";
-       }
-       $dailyWage = $empWagePerHour * $fullDayHour;
-       echo "Daily employee wage is :". $dailyWage."\n";
+class EmpWages
+{
+    function welcomeMsg()
+    {
+        echo "Welcome to employee wage problem" . "\n";
+    }
+    function empCheck()
+    {
+        $empAbsent = 0;
+        $empPresent = 1;
+        $empWagePerHour = 20;
+        $fullDayHour = 8;
+        $number = rand(0, 1);
+        if ($number == 1) {
+            echo "Employee is present" . "\n";
+        } else {
+            echo "Employee is Absent" . "\n";
+        }
+        $dailyWage = $empWagePerHour * $fullDayHour;
+        echo "Daily employee wage is :" . $dailyWage . "\n";
+    }
+    function partTime()
+    {
+        $partTimeEmpWagePerHour = 15;
+        $partTimeHour = 8;
+        $totalPartTimeWage = $partTimeEmpWagePerHour * $partTimeHour;
+        echo "part Time employee wage is :" . $totalPartTimeWage . "\n";
+    }
 
-   }
-   function partTime(){
-       $partTimeEmpWagePerHour = 15;
-       $partTimeHour =8;
-       $totalPartTimeWage = $partTimeEmpWagePerHour * $partTimeHour;  
-       echo "part Time employee wage is :" .$totalPartTimeWage."\n";
-   }
-   
-function attendance(){
-    $number= readline("press 1 for Daily Wage \n press 2 for partTime");
-     switch($number){
-         case 1 :
-            $this->empCheck();
-            break;
-            case 2 :
+    function attendance()
+    {
+        $number = readline("press 1 for Daily Wage \n press 2 for partTime");
+        switch ($number) {
+            case 1:
+                $this->empCheck();
+                break;
+            case 2:
                 $this->partTime();
                 break;
-                default :
+            default:
                 echo "enter a valid input";
                 break;
-     }
-}
+        }
+    }
+    function wageForMonth()
+    {
+        $workingDaysOfMonth = 20;
+        $dailyWage = 160;
+        $wageForMonth = $workingDaysOfMonth * $dailyWage;
+        echo "wage for month = " . $wageForMonth;
+    }
 }
 
 $empWage = new EmpWages();
@@ -51,5 +61,6 @@ $partTimeWage = new EmpWages();
 $partTimeWage->partTime();
 $attend = new EmpWages();
 $attend->attendance();
-
+$empWageForMonth = new EmpWages();
+$empWageForMonth->wageForMonth();
 ?>
