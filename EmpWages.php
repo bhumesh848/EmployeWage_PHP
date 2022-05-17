5b9f07935f2a6e2ec0143d95e48d359ebbea40a9
unknown <bhumesh.ranjaneb598@gmail.com>
2022-05-17 12:17:30 +0530

    Added : UC1

--- EmpWages.php
+++ EmpWages.php
@@ -2,10 +2,26 @@
 
 class EmpWages{
    function welcomeMsg(){
-       echo "Welcome to employee wage problem";
+       echo "Welcome to employee wage problem"."\n";
+   }
+   function empCheck(){
+       $empAbsent = 0;
+       $empPresent = 1;
+       $number =rand(0,1);
+       if($number==1){
+           echo "Employee is present";
+       }
+       else{
+           echo "Employee is Absent";
+       }
+
    }
 }
 $empWage = new EmpWages();
 $empWage->welcomeMsg();
+$AttendanceCheck = new EmpWages();
+$AttendanceCheck->empCheck();
+
+
 
 ?>
