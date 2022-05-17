aa523f0f4ed37e5c3b2b6f0b1f93fdc48ec38d99
unknown <bhumesh.ranjaneb598@gmail.com>
2022-05-17 14:42:45 +0530

    Added : UC3 Daily wage

--- EmpWages.php
+++ EmpWages.php
@@ -7,13 +7,17 @@ class EmpWages{
    function empCheck(){
        $empAbsent = 0;
        $empPresent = 1;
+       $empWagePerHour = 20;
+       $fullDayHour = 8;
        $number =rand(0,1);
        if($number==1){
-           echo "Employee is present";
+           echo "Employee is present"."\n";
        }
        else{
-           echo "Employee is Absent";
+           echo "Employee is Absent"."\n";
        }
+       $dailyWage = $empWagePerHour * $fullDayHour;
+       echo "Daily employee wage is :". $dailyWage;
 
    }
 }
