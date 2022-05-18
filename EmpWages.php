22ae21a51b194a2e530fcd3c92b6e5443757051f
unknown <bhumesh.ranjaneb598@gmail.com>
2022-05-18 12:49:30 +0530

    Refactored: UC4

--- EmpWages.php
+++ EmpWages.php
@@ -1,55 +1,38 @@
 <?php
 
-class EmpWages{
-   function welcomeMsg(){
-       echo "Welcome to employee wage problem"."\n";
-   }
-   function empCheck(){
-       $empAbsent = 0;
-       $empPresent = 1;
-       $empWagePerHour = 20;
-       $fullDayHour = 8;
-       $number =rand(0,1);
-       if($number==1){
-           echo "Employee is present"."\n";
-       }
-       else{
-           echo "Employee is Absent"."\n";
-       }
-       $dailyWage = $empWagePerHour * $fullDayHour;
-       echo "Daily employee wage is :". $dailyWage."\n";
+class EmpWages
+{
+    const PART_TIME_HOUR = 4;
+    const FULL_DAY_HOUR = 8;
+    const EMP_WAGE_PER_HOUR = 20;
+    const FULL_TIME = 1;
+    const PART_TIME = 2;
+    function welcomeMsg()
+    {
+        echo "Welcome to employee wage problem" . "\n";
+    }
+    function empCheck()
+    {
+        $dailyHour = 0;
 
-   }
-   function partTime(){
-       $partTimeEmpWagePerHour = 15;
-       $partTimeHour =8;
-       $totalPartTimeWage = $partTimeEmpWagePerHour * $partTimeHour;  
-       echo "part Time employee wage is :" .$totalPartTimeWage."\n";
-   }
-   
-function attendance(){
-    $number= readline("press 1 for Daily Wage \n press 2 for partTime");
-     switch($number){
-         case 1 :
-            $this->empCheck();
-            break;
-            case 2 :
-                $this->partTime();
+        $number = rand(0, 2);
+        switch ($number) {
+            case EmpWages::FULL_TIME:
+                echo "Employee is present" . "\n";
+                $dailyHour = EmpWages::FULL_DAY_HOUR;
                 break;
-                default :
-                echo "enter a valid input";
+            case EmpWages::PART_TIME:
+                echo "Employee is part time";
+                $dailyHour = EmpWages::PART_TIME_HOUR;
                 break;
-     }
+            default:
+                echo "Employee is Absent" . "\n";
+        }
+        $dailyWage = EmpWages::EMP_WAGE_PER_HOUR * $dailyHour;
+        echo "Daily employee wage is :" . $dailyWage . "\n";
+    }
 }
-}
-
 $empWage = new EmpWages();
 $empWage->welcomeMsg();
-$AttendanceCheck = new EmpWages();
-$AttendanceCheck->empCheck();
-$partTimeWage = new EmpWages();
-$partTimeWage->partTime();
-$attend = new EmpWages();
-$attend->attendance();
-
+$empWage->empCheck();
 ?>
