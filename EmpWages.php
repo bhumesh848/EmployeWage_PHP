4cb84b12d150a61ffd298185916fc130ad9291d2
unknown <bhumesh.ranjaneb598@gmail.com>
2022-05-19 10:53:08 +0530

    Added UC8 Parameterized function

--- EmpWages.php
+++ EmpWages.php
@@ -4,20 +4,18 @@ class EmpWages
 {
     const PART_TIME_HOUR = 4;
     const FULL_DAY_HOUR = 8;
-    const EMP_WAGE_PER_HOUR = 20;
     const FULL_TIME = 1;
     const PART_TIME = 2;
-    const WORKING_DAYS = 20;
     const MAX_HOURS = 100;
     function welcomeMsg()
     {
         echo "Welcome to employee wage problem" . "\n";
     }
-    function empCheck()
+    function empCheck($WORKING_DAYS_PER_MONTH, $WORKING_HOURS_PER_MONTH, $WAGE_PER_HR)
     {
         $dailyHour = 0;
         $day = 0;
-         while($day<EmpWages::WORKING_DAYS && $dailyHour <=EmpWages::MAX_HOURS){
+         while($day<=$WORKING_DAYS_PER_MONTH && $dailyHour <=$WORKING_HOURS_PER_MONTH){
             $number = rand(0, 2);
             switch ($number) {
                 case EmpWages::FULL_TIME:
@@ -34,13 +32,29 @@ class EmpWages
             $day++;
          }
         
-        $monthlyWage = EmpWages::EMP_WAGE_PER_HOUR * $dailyHour;
+        $monthlyWage = $WAGE_PER_HR * $dailyHour;
         echo "Daily employee wage is :" . $monthlyWage . "\n";
         echo "number of  total working days =". $day."\n";
         echo "number of working hours = $dailyHour";
         
     }
+    function company()
+    {
+        $name = readline('Enter Name of Company: ');
+        echo "Employee Wage Computation For".$name."\n";
+        $WORKING_DAYS_PER_MONTH = readline('Enter Max Working Days Per Month: ');
+        $WORKING_HOURS_PER_MONTH = readline('Enter Max Working Hours Per Month: ');
+        $WAGE_PER_HR = readline('Enter Employee Wage Per Hour: ');
+        $this->empCheck($WORKING_DAYS_PER_MONTH, $WORKING_HOURS_PER_MONTH, $WAGE_PER_HR);
+    }
 }
-$empWage = new EmpWages();
-$empWage->welcomeMsg();
-$empWage->empCheck();
+
+
+
+// $empWage = new EmpWages();
+// $empWage->welcomeMsg();
+// $empWage->empCheck();
+$company1 = new EmpWages();
+echo $company1->company();
+$company2 = new EmpWages();
+$company2->company();
