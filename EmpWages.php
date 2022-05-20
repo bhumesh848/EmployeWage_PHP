be0b78f6f8f8fe2c3a65f30f30f8837103982553
unknown <bhumesh.ranjaneb598@gmail.com>
2022-05-20 10:38:11 +0530

    Added: UC9 Instance Variable

--- EmpWages.php
+++ EmpWages.php
@@ -2,11 +2,25 @@
 
 class EmpWages
 {
-    const PART_TIME_HOUR = 4;
-    const FULL_DAY_HOUR = 8;
-    const FULL_TIME = 1;
-    const PART_TIME = 2;
-    const MAX_HOURS = 100;
+    public $PART_TIME_HOUR = 4;
+    public $FULL_DAY_HOUR = 8;
+    public $FULL_TIME = 1;
+    public $PART_TIME = 2;
+    public $MAX_HOURS = 100;
+
+    private $WORKING_DAYS_PER_MONTH;
+    private $WORKING_HOURS_PER_MONTH;
+    private $WAGE_PER_HR;
+
+    function company()
+    {
+        $name = readline('Enter Name of Company: ');
+        echo "Employee Wage Computation For" . $name . "\n";
+        $this->WORKING_DAYS_PER_MONTH = readline('Enter Max Working Days Per Month: ');
+        $this->WORKING_HOURS_PER_MONTH = readline('Enter Max Working Hours Per Month: ');
+        $this->WAGE_PER_HR = readline('Enter Employee Wage Per Hour: ');
+        $this->empCheck($this->WORKING_DAYS_PER_MONTH,$this->WORKING_HOURS_PER_MONTH, $this->WAGE_PER_HR);
+    }
     function welcomeMsg()
     {
         echo "Welcome to employee wage problem" . "\n";
@@ -15,38 +29,29 @@ class EmpWages
     {
         $dailyHour = 0;
         $day = 0;
-         while($day<=$WORKING_DAYS_PER_MONTH && $dailyHour <=$WORKING_HOURS_PER_MONTH){
+        while ($day <= $WORKING_DAYS_PER_MONTH && $dailyHour <= $WORKING_HOURS_PER_MONTH) {
             $number = rand(0, 2);
             switch ($number) {
-                case EmpWages::FULL_TIME:
+                case $this->FULL_TIME:
                     echo "Employee is present" . "\n";
-                    $dailyHour += EmpWages::FULL_DAY_HOUR;
+                    $dailyHour += $this->FULL_DAY_HOUR;
                     break;
-                case EmpWages::PART_TIME:
+                case $this->PART_TIME:
                     echo "Employee is part time";
-                    $dailyHour += EmpWages::PART_TIME_HOUR;
+                    $dailyHour += $this->PART_TIME_HOUR;
                     break;
                 default:
                     echo "Employee is Absent" . "\n";
             }
             $day++;
-         }
-        
+        }
+
         $monthlyWage = $WAGE_PER_HR * $dailyHour;
         echo "Daily employee wage is :" . $monthlyWage . "\n";
-        echo "number of  total working days =". $day."\n";
+        echo "number of  total working days =" . $day . "\n";
         echo "number of working hours = $dailyHour";
-        
     }
-    function company()
-    {
-        $name = readline('Enter Name of Company: ');
-        echo "Employee Wage Computation For".$name."\n";
-        $WORKING_DAYS_PER_MONTH = readline('Enter Max Working Days Per Month: ');
-        $WORKING_HOURS_PER_MONTH = readline('Enter Max Working Hours Per Month: ');
-        $WAGE_PER_HR = readline('Enter Employee Wage Per Hour: ');
-        $this->empCheck($WORKING_DAYS_PER_MONTH, $WORKING_HOURS_PER_MONTH, $WAGE_PER_HR);
-    }
+   
 }
 
 
@@ -55,6 +60,6 @@ class EmpWages
 // $empWage->welcomeMsg();
 // $empWage->empCheck();
 $company1 = new EmpWages();
-echo $company1->company();
+$company1->company();
 $company2 = new EmpWages();
 $company2->company();
